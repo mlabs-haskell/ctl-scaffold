b0e3e5f11cdbea805c9a826cea53fd4c6fef25d6
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-04-19 14:39:40 +0700

    Simplify setup

--- nix/default.nix
+++ nix/default.nix
@@ -58,7 +58,38 @@ in
     };
   };
 
-  devShell = import ./dev-shell.nix {
-    inherit pkgs system inputs nodeModules easy-ps nodejs;
-  };
+  devShell =
+    pkgs.mkShell
+      {
+        buildInputs = with easy-ps; [
+          easy-ps.purs-0_14_5
+          spago
+          purs-tidy
+          purescript-language-server
+          pscid
+          spago2nix
+          pkgs.nodePackages.node2nix
+          nodejs
+          pkgs.nixpkgs-fmt
+          pkgs.fd
+        ];
+
+        shellHook = ''
+          __ln-node-modules () {
+            local modules=./node_modules
+            if test -L "$modules"; then
+              rm "$modules";
+            elif test -e "$modules"; then
+              echo 'refusing to overwrite existing (non-symlinked) `node_modules`'
+              exit 1
+            fi
+
+            ln -s ${nodeModules}/lib/node_modules "$modules"
+          }
+
+          __ln-node-modules
+
+          export PATH="${nodeModules}/bin:$PATH"
+        '';
+      };
 }
